2b50016a732ccd510f18dd6eedb0d2cffa29e183
Margarita Dagla <margaritadagla@hotmail.com>
2023-02-06 20:40:37 +0000

    controllers and views done

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  before_action :set_list, only: [:new, :create]
+
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @bookmark.list = @list
+    @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@bookmark.list)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list), status: :see_other
+  end
+
+  private
+
+  def set_list
+    @list = List.find(params[:list_id])
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+
+  def bookmark_params
+    params.require(:bookmark).permit(:content, :list_id, :movie_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :lists, only: [ :index, :show, :new, :create ] do
+    resources :bookmarks, only: [ :new, :create ]
+  end
+  resources :bookmarks, only: [ :destroy ]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
